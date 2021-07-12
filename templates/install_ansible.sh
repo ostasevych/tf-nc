1e8f7744e33835cdcd3ff9f4c84188e98d581793
Oleksiy Stasevych <oleksiy.stasevych@gmail.com>
2021-07-13 00:08:08 +0300

    updated playbooks

--- templates/install_ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt-get update && apt-get -qq install python3 -y && apt-get upgrade python3
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip git
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade ansible
-echo \"Running Ansible in `pwd`\"
-
